4cfbda28f4b402ae90f2a77294d306dbdd8c4b62
BogdanGlazkov <bglazkov@i.ua>
2023-08-17 23:12:25 +0300

    add ApplyButton component

---
+++ src/components/ApplyButton/ApplyButton.jsx
@@ -0,0 +1,25 @@
+'use client';
+
+import PropTypes from 'prop-types';
+
+import data from 'dictionaries/uk.json';
+
+export const ApplyButton = ({ isFree }) => {
+  return (
+    <button
+      className={`${
+        isFree
+          ? 'h-[35px] md:w-[211px] md:h-[53px] xl:w-[305px] xl:h-[92px] xl:text-large36'
+          : 'h-[43px] md:h-[61px]'
+      } w-full flex items-center justify-center bg-accent border-2 border-accent rounded-10 text-middle md:text-large font-medium cursor-pointer duration-300 hover:bg-white focus:bg-white`}
+      type="button"
+      onClick={() => {}}
+    >
+      {data.applyButtonLabel}
+    </button>
+  );
+};
+
+ApplyButton.propTypes = {
+  isFree: PropTypes.bool.isRequired,
+};
