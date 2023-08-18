7405e194bb4f425dd9ef626c33fa37d4b86fadbc
ginkosoul <ginkosoul@gmail.com>
2023-08-18 22:27:23 +0300

    update: added svg as :after in title

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,27 +1,20 @@
 import PropTypes from 'prop-types';
 
-export const SectionTitle = ({ text }) => {
+export const SectionTitle = ({ className, text }) => {
   return (
-    <>
-      <h2 className="text-center mb-[4px] md:mb-[8px] xl:mb-[12px]">{text}</h2>
-      <svg
-        width="24"
-        height="24"
-        className="mx-auto w-[12px] h-[12px] md:w-[16px] md:h-[16px] xl:w-[24px] xl:h-[24px]"
-        aria-label="Arrow down"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M20.7281 4.36407L12 13.0922L3.27188 4.36407L0 7.63595L12 19.6359L24 7.63595L20.7281 4.36407Z"
-          fill="#222222"
-        />
-      </svg>
-    </>
+    <h2
+      className={`text-center mb-[16px] md:mb-[36px] ${className}
+      after:content-[var(--arrow-image)] after:mx-auto
+      after:w-[12px] after:md:w-[16px] after:xl:w-[24px] 
+      after:h-[12px] after:md:h-[16px] after:xl:h-[24px] 
+      flex flex-col gap-[4px] md:gap-[8px] xl:gap-[12px]`}
+    >
+      {text}
+    </h2>
   );
 };
 
 SectionTitle.propTypes = {
+  className: PropTypes.string,
   text: PropTypes.string.isRequired,
 };
