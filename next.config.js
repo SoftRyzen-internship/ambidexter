3ed323457d143c193d7b5d26ef8ecf0f8fb82d56
melyasanya <werbolom@gmail.com>
2023-08-20 20:20:07 +0300

    corrections

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   eslint: {
     dirs: ['src'], // Only run ESLint on the 'src' directory during production builds (next build)
   },
+
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
