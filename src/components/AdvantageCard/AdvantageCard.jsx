ddd74acbf4b236b8b64dcee5e50c31dd682a99df
ginkosoul <ginkosoul@gmail.com>
2023-08-21 10:59:01 +0300

    updaete: added marker as :before in list

--- src/components/AdvantageCard/AdvantageCard.jsx
+++ src/components/AdvantageCard/AdvantageCard.jsx
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 export const AdvantageCard = ({ count, text, list: advantagesList }) => {
   return (
     <li
-      className={`py-4 xl:py-8 px-3 md:px-4 bg-accent rounded-10 ${
-        advantagesList && 'row-span-2'
-      }`}
+      className={
+        advantagesList
+          ? 'py-4 xl:py-8 px-3 md:px-4 bg-accent rounded-10 max-w-full row-span-2'
+          : 'py-4 xl:py-8 px-3 md:px-4 bg-accent rounded-10 max-w-full'
+      }
     >
       <p
-        className={`text-small xl:text-large font-medium flex items-center gap-2 ${
-          advantagesList && 'mb-2'
-        }`}
+        className={
+          advantagesList
+            ? 'text-small xl:text-large font-medium flex items-center gap-2 mb-2'
+            : 'text-small xl:text-large font-medium flex items-center gap-2'
+        }
       >
         <span className="text-large64/[1] md:text-large70 xl:text-large140/[143px] font-medium">
           {count}
@@ -21,10 +25,10 @@ export const AdvantageCard = ({ count, text, list: advantagesList }) => {
         <ul>
           {advantagesList.map((text, idx) => (
             <li
-              className="text-small/[1.53] xl:text-large/[1.486] font-medium"
+              className="text-small/[1.6] xl:text-large/[1.486] font-medium flex items-center before:block
+              before:bg-black before:mr-1.5 md:before:mr-2 before:h-1 before:w-1 xl:before:h-1.5 xl:before:w-1.5"
               key={idx}
             >
-              {'▪ '}
               {text}
             </li>
           ))}
