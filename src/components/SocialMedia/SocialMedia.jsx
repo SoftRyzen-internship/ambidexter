7275987ee34c98ae7a45e5c429e2a7b55dd8ee2e
melyasanya <werbolom@gmail.com>
2023-08-21 23:26:07 +0300

    corrections

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -4,84 +4,72 @@ import InstagramIcon from 'public/icons/instagram.svg';
 import YoutubeIcon from 'public/icons/youtube.svg';
 import TikTokIcon from 'public/icons/tiktok.svg';
 
-export const SocialMedia = ({ section }) => {
+export const SocialMedia = ({ data, section }) => {
   const circleClassName = section === 'menu' || section === 'hero';
+
+  const socialMediaIcons = {
+    Instagram: (
+      <InstagramIcon
+        className={
+          circleClassName
+            ? 'socialMediaIcon fill-bgColor '
+            : 'socialMediaIcon fill-accent '
+        }
+      />
+    ),
+    Youtube: (
+      <YoutubeIcon
+        className={
+          circleClassName
+            ? 'socialMediaIcon stroke-bgColor [&>*:first-child]:fill-bgColor'
+            : 'socialMediaIcon stroke-accent [&>*:first-child]:fill-accent'
+        }
+      />
+    ),
+    Tiktok: (
+      <TikTokIcon
+        className={
+          circleClassName
+            ? 'socialMediaIcon  stroke-bgColor'
+            : 'socialMediaIcon stroke-accent '
+        }
+      />
+    ),
+  };
 
   return (
     <ul className="flex  gap-[8px] md:gap-[16px] xl:gap-[24px]">
-      <li
-        className={
-          circleClassName
-            ? 'circleSocialItems hover:bg-accent focus:bg-accent'
-            : 'circleSocialItems hover:bg-white focus:bg-white'
-        }
-      >
-        <a
-          href="https://www.instagram.com/ambidexter.school/"
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-          aria-label="Instagram icon"
-          className="w-full h-full flex justify-center items-center"
-        >
-          <InstagramIcon
+      {data.map((el, index) => {
+        return (
+          <li
+            key={index}
             className={
               circleClassName
-                ? 'socialMediaIcon fill-bgColor '
-                : 'socialMediaIcon fill-accent '
+                ? 'circleSocialItems hover:bg-accent focus:bg-accent'
+                : 'circleSocialItems hover:bg-white focus:bg-white'
             }
-          />
-        </a>
-      </li>
-      <li
-        className={
-          circleClassName
-            ? 'circleSocialItems hover:bg-accent focus:bg-accent'
-            : 'circleSocialItems hover:bg-white focus:bg-white'
-        }
-      >
-        <a
-          href="https://www.youtube.com/@Ambidexterschool"
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-          aria-label="Youtube icon"
-          className="w-full h-full flex justify-center items-center"
-        >
-          <YoutubeIcon
-            className={
-              circleClassName
-                ? 'socialMediaIcon stroke-bgColor [&>*:first-child]:fill-bgColor'
-                : 'socialMediaIcon stroke-accent [&>*:first-child]:fill-accent'
-            }
-          />
-        </a>
-      </li>
-      <li
-        className={
-          circleClassName
-            ? 'circleSocialItems hover:bg-accent focus:bg-accent'
-            : 'circleSocialItems hover:bg-white focus:bg-white'
-        }
-      >
-        <a
-          href="http://tiktok.com/@ambidexterschool"
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-          aria-label="TikTok icon"
-          className="w-full h-full flex justify-center items-center"
-        >
-          <TikTokIcon
-            className={
-              circleClassName
-                ? 'socialMediaIcon  stroke-bgColor'
-                : 'socialMediaIcon stroke-accent '
-            }
-          />
-        </a>
-      </li>
+          >
+            <a
+              href={el.link}
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              aria-label={el.ariaLabel}
+              className="w-full h-full flex justify-center items-center rounded-[50%]"
+            >
+              {socialMediaIcons[el.name]}
+            </a>
+          </li>
+        );
+      })}
     </ul>
   );
 };
 
 SocialMedia.propTypes = {
   section: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    ariaLabel: PropTypes.string.isRequired,
+  }),
 };
