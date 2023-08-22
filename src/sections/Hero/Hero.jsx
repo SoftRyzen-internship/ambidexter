6b88ae560f9c18bb63c06e7fe0189562c3333a2d
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-08-22 13:11:28 +0300

    add hero

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -5,15 +5,18 @@ import { Container } from '@/components';
 import heroImg from 'public/images/hero.png';
 
 export const Hero = ({ data }) => {
-  const { speciality, title } = data;
+  const {
+    speciality: [leftList, rightList],
+    title,
+  } = data;
 
   return (
-    <section className="bg-heroBg bg-no-repeat bg-center bg-cover">
+    <section className="bg-heroBg bg-no-repeat bg-top bg-cover pt-20 md:pt-[75px] xl:pt-[93px] min-h-[420px] md:min-h-[609px] xl:min-h-[1044px] z-[-3]">
       <Container>
-        <div className="relative flex justify-center md:text-base xl:text-large font-medium">
-          <div className="flex flex-col md:gap-28 xl:gap-64">
+        <div className="relative flex justify-center md:text-base xl:text-large font-medium w-full h-full">
+          <div className="flex flex-col md:gap-28 xl:gap-64 min-w-max absolute top-0 left-0 md:top-[53px] xl:top-[106px] z-10">
             <ul className="flex flex-col gap-2 md:gap-3 xl:gap-6">
-              {speciality[0].map((element, index) => (
+              {leftList.map((element, index) => (
                 <li key={index}>
                   <p>{element}</p>
                 </li>
@@ -21,14 +24,16 @@ export const Hero = ({ data }) => {
             </ul>
             <div className="hidden md:block w-[128px] h-[32px] bg-black"></div>
           </div>
-          <div className="text-end flex flex-col gap-12 md:gap-[68px] xl:gap-[120px]">
-            <h1 className="text-[32px] md:text-large64 xl:text-large134/[1] ">
+          <div className="text-end flex flex-col items-end gap-12 md:gap-[68px] xl:gap-[120px] absolute top-[61px] right-0 md:top-[42px] xl:top-[62px] z-20">
+            <h1 className="text-[32px]/[1] md:text-large64/[1] xl:text-large134/[1] max-w-min">
               {title}
             </h1>
             <ul className="flex flex-col gap-2 md:gap-3 xl:gap-6">
-              {speciality[1].map((element, index) => (
+              {rightList.map((element, index) => (
                 <li key={index}>
-                  <p>{element}</p>
+                  {element.map(el => (
+                    <p key={el}>{el}</p>
+                  ))}
                 </li>
               ))}
             </ul>
@@ -37,9 +42,9 @@ export const Hero = ({ data }) => {
             src={heroImg}
             alt="Галушка Кирило"
             priority={true}
-            width={709}
-            height={933}
-            className="absolute z-[-1] max-w-[227px] md:max-w-[398px] xl:max-w-[709px]"
+            width={737}
+            height={951}
+            className="absolute top-9 md:top-0 md:left-1/2 md:translate-x-[-35%] xl:translate-x-[-45%] z-0 max-w-[235px] md:max-w-[414px] xl:max-w-[737px]"
           />
         </div>
       </Container>
@@ -50,6 +55,6 @@ export const Hero = ({ data }) => {
 Hero.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    speciality: PropTypes.arrayOf.isRequired,
+    speciality: PropTypes.arrayOf(PropTypes.array.isRequired).isRequired,
   }).isRequired,
 };
