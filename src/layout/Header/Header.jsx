01a942eea57351e9dfcb2e73c2a8ef83e234a6d0
melyasanya <werbolom@gmail.com>
2023-08-23 00:07:17 +0300

    correction to open modal

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,18 +1,19 @@
 'use client';
 import { Container, FeedbackForm, LocaleSwitcher, Logo } from '@/components';
+import { ModalForm } from '@/components/ModalForm/ModalForm';
 import { useState } from 'react';
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleModal = () => {
-    isOpen ? setIsOpen(false) : setIsOpen(true)
+    isOpen ? setIsOpen(false) : setIsOpen(true);
   };
 
   return (
     <header>
       <Container>
         {isOpen ? (
-          <FeedbackForm toggleModal={toggleModal}/>
+          <ModalForm toggleModal={toggleModal} />
         ) : (
           <>
             <Logo isColored={true} />
