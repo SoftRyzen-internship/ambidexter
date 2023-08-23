1f919247de5e3bcd43f22f8ba262b8032dc9347a
BogdanGlazkov <bglazkov@i.ua>
2023-08-23 12:55:49 +0300

    fix Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,7 @@
-'use client';
-
-import { ThreeDots } from 'react-loader-spinner';
-
 export const Loader = () => {
   return (
     <div className="flex items-center justify-center">
-      <ThreeDots height="80" width="80" color="green" ariaLabel="loading" />
+      <div className="spinner"></div>
     </div>
   );
 };
