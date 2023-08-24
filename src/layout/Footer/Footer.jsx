7226317e405a9b075b1449c9d5a3db2e09211256
BogdanGlazkov <bglazkov@i.ua>
2023-08-24 16:20:30 +0300

    fix hover color

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -20,7 +20,11 @@ export const Footer = ({ contacts, navBar, socialMedia, locale }) => {
         <div className="h-4 mb-4 md:mb-8 xl:mb-12 md:flex md:items-center md:justify-between">
           <Logo isColored={false} locale={locale} />
           {!isMobile && (
-            <NavBar className="md:text-small xl:text-large" data={navBar} />
+            <NavBar
+              className="md:text-small xl:text-large"
+              data={navBar}
+              isFooter={true}
+            />
           )}
         </div>
 
