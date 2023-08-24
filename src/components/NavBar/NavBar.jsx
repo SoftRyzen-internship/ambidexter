9ca22832d6595111d79bd88c205c1de06e177b25
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-08-24 11:56:15 +0300

    bugs navbar html

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,17 +3,19 @@ import { LinkNavBar } from '@/components';
 
 export const NavBar = ({ className, data, handleMenuToggle }) => {
   return (
-    <nav
-      className={`${className} md:flex uppercase md:py-3  items-center gap-4 xl:gap-6 font-medium leading-normal xl:text-[24px] text-black `}
-    >
-      {data.map(({ label, link }) => (
-        <LinkNavBar
-          key={label}
-          link={link}
-          label={label}
-          handleMenuToggle={handleMenuToggle}
-        />
-      ))}
+    <nav>
+      <ul
+        className={`${className} md:flex uppercase md:py-3  items-center gap-4 xl:gap-6 font-medium leading-normal xl:text-[24px] text-black `}
+      >
+        {data.map(({ label, link }) => (
+          <LinkNavBar
+            key={label}
+            link={link}
+            label={label}
+            handleMenuToggle={handleMenuToggle}
+          />
+        ))}
+      </ul>
     </nav>
   );
 };
