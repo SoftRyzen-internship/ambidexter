2541ad3a9af320887c2443f8729e9435243fd14e
Nadiia <holineinadia@gmail.com>
2023-08-25 16:57:37 +0300

    fix header, locale button, add aria-label

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import PropTypes from 'prop-types';
 import {
@@ -15,9 +15,17 @@ import {
 export const Header = ({ navBar, socialNetworks, socialMedia, locale }) => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
-  const [isScrollHeight, setIsScroll] = useState(0);
+  const [scrollHeight, setScrollHeight] = useState(null);
+  const [isHydrated, setIsHydrated] = useState(false);
 
   const mobile = useMediaQuery({ maxWidth: 767 });
+
+  useLayoutEffect(() => {
+    if (scrollHeight === null) {
+      setScrollHeight(window.scrollY);
+    }
+    setIsHydrated(true);
+  }, [scrollHeight]);
 
   useEffect(() => {
     isOpenMenu
@@ -41,55 +49,59 @@ export const Header = ({ navBar, socialNetworks, socialMedia, locale }) => {
     };
   }, []);
 
+  const listenCallback = useCallback(() => {
+    console.log(window.scrollY);
+    setScrollHeight(window.scrollY);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('scroll', listenCallback);
+
+    return () => {
+      document.removeEventListener('scroll', listenCallback);
+    };
+  }, [listenCallback, scrollHeight, setScrollHeight]);
+
   const handleMenuToggle = () => {
     setIsOpenMenu(prev => !prev);
   };
 
-  useEffect(() => {
-    document.addEventListener('scroll', function () {
-      setIsScroll(window.scrollY);
-    });
-  }, [isScrollHeight]);
-
-  const returnBlur = () => {
-    if (isScrollHeight > 0) {
-      return true;
-    }
-    return false;
-  };
-
   return (
-    <header
-      className={`z-30 flex items-center h-[68px] md:h-[63px] xl:h-[93px]  fixed w-full  ${
-        returnBlur() && 'backdrop-blur-[100px]'
-      }  bg-headerBg `}
-    >
-      <Container className="flex items-center justify-between md:h-[39px] xl:h-[29px]">
-        <Logo isColored={true} locale={locale} />
+    <>
+      {isHydrated && (
+        <header
+          className={`z-30 flex items-center h-[68px] md:h-[63px] xl:h-[93px]  fixed w-full  ${
+            scrollHeight > 0 && 'backdrop-blur-[100px] bg-headerBg'
+          }`}
+        >
+          <Container className="flex items-center justify-between md:h-[39px] xl:h-[29px]">
+            <Logo isColored={true} locale={locale} />
 
-        {!isMobile && (
-          <NavBar data={navBar} className="hidden md:block text-[12px]" />
-        )}
-        {!isMobile && <LocaleSwitcher className="hidden md:block " />}
+            {!isMobile && (
+              <NavBar data={navBar} className="hidden md:block text-[12px]" />
+            )}
+            {!isMobile && <LocaleSwitcher className="hidden md:block " />}
 
-        {isMobile && (
-          <ButtonMenuToggle
-            isOpenMenu={isOpenMenu}
-            handleMenuToggle={handleMenuToggle}
-          />
-        )}
+            {isMobile && (
+              <ButtonMenuToggle
+                isOpenMenu={isOpenMenu}
+                handleMenuToggle={handleMenuToggle}
+              />
+            )}
 
-        {isMobile && (
-          <MobileMenu
-            handleMenuToggle={handleMenuToggle}
-            navBar={navBar}
-            socialNetworks={socialNetworks}
-            isModalShow={isOpenMenu}
-            socialMedia={socialMedia}
-          />
-        )}
-      </Container>
-    </header>
+            {isMobile && (
+              <MobileMenu
+                handleMenuToggle={handleMenuToggle}
+                navBar={navBar}
+                socialNetworks={socialNetworks}
+                isModalShow={isOpenMenu}
+                socialMedia={socialMedia}
+              />
+            )}
+          </Container>
+        </header>
+      )}
+    </>
   );
 };
 
