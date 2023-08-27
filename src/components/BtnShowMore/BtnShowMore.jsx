480bc4f251e7792486496eabbe6d14c42f1e3925
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-08-27 19:53:42 +0300

    fix hover on btnShowMore in CardCourses

--- src/components/BtnShowMore/BtnShowMore.jsx
+++ src/components/BtnShowMore/BtnShowMore.jsx
@@ -5,7 +5,7 @@ export const BtnShowMore = ({ title, link }) => {
   return (
     <Link
       href={link}
-      className="flex items-center justify-center bg-bgColor hover:bg-white focus:bg-white h-[51px] xl:h-[61px] rounded-10 text-middle font-medium min-w-full xl:text-large duration-300 xl:px-20 xl:min-w-max"
+      className="flex items-center justify-center bg-bgColor hover:bg-white hover:font-bold focus:bg-white focus:font-bold h-[51px] xl:h-[61px] rounded-10 text-middle font-medium min-w-full xl:text-large duration-300 xl:px-20 xl:min-w-max"
     >
       {title}
     </Link>
