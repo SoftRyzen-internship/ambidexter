b47fe7e040f25c669f41070628a269d755c8a906
Nadiia <holineinadia@gmail.com>
2023-08-30 11:24:53 +0300

    fix bug with generated on demand

--- src/app/[locale]/[skill]/page.jsx
+++ src/app/[locale]/[skill]/page.jsx
@@ -3,6 +3,17 @@ import { getDictionary } from '@/utils/getDictionary';
 
 import { CoursePageList, FreeClass } from '@/sections';
 
+export const dynamicParams = false;
+
+export async function generateStaticParams({ params }) {
+  const { locale } = params;
+
+  return [
+    { locale: locale, skill: 'oratory-skill' },
+    { locale: locale, skill: 'acting-skill' },
+  ];
+}
+
 export async function generateMetadata({ params: { locale, skill } }) {
   const metaDictionary = await getMetaByLocale(locale);
 
