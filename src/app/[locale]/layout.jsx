27709803ece7b0f1c1c082895daad573194e1c57
Nadiia <holineinadia@gmail.com>
2023-08-31 11:54:00 +0300

    test favicons

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -27,6 +27,39 @@ export async function generateMetadata({ params }) {
       //  canonical: 'https://ambidexter.vercel.app/',
       // },
     },
+    icons: {
+      icon: [
+        {
+          url: '/favicon/favicon.ico',
+        },
+        {
+          url: '/favicon/favicon-16x16.png',
+          sizes: '16x16',
+          type: 'image/png',
+        },
+        {
+          url: '/favicon/favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png',
+        },
+        {
+          url: '/favicon/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          url: '/favicon/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+      shortcut: '/favicon/favicon.ico',
+      apple: '/favicon/apple-touch-icon.png',
+      other: {
+        rel: '/favicon/apple-touch-icon.png',
+        url: '/favicon/apple-touch-icon.png',
+      },
+    },
   };
 }
 
