6cd5b0437cd932922ba1704d91eebcad05285140
Nadiia <holineinadia@gmail.com>
2023-09-03 14:48:41 +0300

    add language to cookies and change middleware logic

--- src/middleware.js
+++ src/middleware.js
@@ -6,21 +6,46 @@ export function middleware(request) {
   // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname;
 
-  const pathnameIsMissingLocale = i18n.locales.every(
-    locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+  const pathnameLocale = i18n.locales.find(
+    locale => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
   );
 
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    return NextResponse.redirect(
-      new URL(
-        `/${i18n.defaultLocale}${
-          pathname.startsWith('/') ? '' : '/'
-        }${pathname}`,
-        request.url,
-      ),
-    );
+  // Redirect if there is no locale in the pathname
+  if (!pathnameLocale) {
+    // Check if there is cookie for locale
+    const localeFromCookies = request.cookies.get('locale')?.value;
+
+    if (localeFromCookies) {
+      const response = NextResponse.redirect(
+        new URL(
+          `/${localeFromCookies}${
+            pathname.startsWith('/') ? '' : '/'
+          }${pathname}`,
+          request.url,
+        ),
+      );
+
+      return response;
+    } else {
+      const response = NextResponse.redirect(
+        new URL(
+          `/${i18n.defaultLocale}${
+            pathname.startsWith('/') ? '' : '/'
+          }${pathname}`,
+          request.url,
+        ),
+      );
+      response.cookies.set('locale', i18n.defaultLocale);
+
+      return response;
+    }
   }
+
+  const response = NextResponse.next();
+
+  response.cookies.set('locale', pathnameLocale);
+
+  return response;
 }
 
 export const config = {
